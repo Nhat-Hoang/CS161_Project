5c5f3c5c7a57a60cdb59f716423702ed221116b5
NhatHoangca <NhatHoangca@users.noreply.github.com>
2018-11-10 13:29:53 +0700

    Version 0.00.20

---
+++ Caro_Gomoku ___/Setting.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#include "console.h"
+
+using namespace std;
+
+// Hide the mouse
+void Nocursortype()
+{
+	CONSOLE_CURSOR_INFO Info;
+	Info.bVisible = FALSE;
+	Info.dwSize = 20;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+}
+// Resize the size of Console window
+void resizeConsole(int width, int height)
+{
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+	MoveWindow(console, r.left, r.top, width, height, TRUE);
+}
---
+++ Caro_Gomoku ___/Setting.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#include "console.h"
+
+void Nocursortype();
+void resizeConsole(int width, int height);
