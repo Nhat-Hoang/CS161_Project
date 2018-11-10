7065be623d28c6f9ea086ce0c4e2a2ac7786f3b6
NhatHoangca <NhatHoangca@users.noreply.github.com>
2018-11-10 13:23:18 +0700

    Version 0.00.10

---
+++ Caro_Gomoku/StartScreen.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+#include <Windows.h>
+#include <conio.h>
+#include <mmsystem.h>
+#include "console.h"
+
+// Start Screen
+int Menu();							// Display the menu to choose user option
+void Start_Screen();				// Display the Start screen
+void Screen();						// Show the start screen without animation
+void Clear_Screen();				// Clear the console then show the start screen
