21ecd016f2530f2a4848d2cfc8428973bb1f0d0a
NhatHoangca <NhatHoangca@users.noreply.github.com>
2018-11-10 15:37:11 +0700

    Version 0.01.00

---
+++ Caro_Gomoku (10.11.2018)/Animation.h
@@ -0,0 +1,6 @@
+#include <Windows.h>
+
+void Execute_1();							// Splash Screen
+void Lose_Animation();						// You Lose :))
+void Win_Animation(int n);					// Player win :(
+void EoBietCaiGiNua(int n);					// Display an instruction
